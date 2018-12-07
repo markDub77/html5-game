6fea150b0b98882fba174011230ceb86c345f2fc
markDub77 <markwilliams1977@gmail.com>
2018-12-06 19:38:08 -0600

    trying to get arcade and p2 working

--- src/main.1.js
+++ src/main.1.js
@@ -7,9 +7,7 @@ var transparent = false;
 var antialias = false;
 var gameWidth = 800;
 var gameHeight = 600;
-var sprite;
-var sprite2;
-var sprites;
+var heroGroup;
 var pad1;
 var indicator;
 var grappleBMD;
@@ -18,313 +16,255 @@ var line;
 var handle1;
 var handle2;
 var graphics;
-var hook;
+var hookSprite;
 var bmd;
-var bg;
-var snakeHeadBdm;
-
-var snakeHead; //head of snake sprite
-var snakeSection = new Array(); //array of sprites that make the snake body sections
-var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
-var numSnakeSections = 50; //number of snake body sections
-//var snakeSpacer = 1; //parameter that sets the spacing between sections
+var hookBmd;
+var chainLink;
+var square;
+var chainLink = new Array(); //array of sprites that make the snake body sections
+var chainPath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
+var numChainLinks = 40; //number of snake body sections
 var cursors;
+var grappleLaunch = false;
+var mainState = {
+
+    preload: function() {
+        game.load.spritesheet('controller-indicator', 'assets/images/controller-indicator.png', 16,16);
+        game.scaleMode = Phaser.ScaleManager.RESIZE;
+        game.input.gamepad.start();  
+        pad1 = game.input.gamepad.pad1;   
+    },
+    create: function() {
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.startSystem(Phaser.Physics.P2JS);
+        game.physics.p2.gravity.y = 1200;
+        game.stage.backgroundColor = '#000';
+        game.world.enableBody = true;
 
 
-var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render }, transparent, antialias);
-
-function preload() {
-    game.load.spritesheet('controller-indicator', 'assets/images/controller-indicator.png', 16,16);
-    game.load.image('arrow', 'assets/images/phaser-dude.png');
-
-    game.scaleMode = Phaser.ScaleManager.RESIZE;
-}
-function create() {
-
-    game.stage.backgroundColor = '#000';
-
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    
-
-    // 1 
-    // snakeHeadBdm = game.add.bitmapData(800, 600);
-    // snakeHeadBdm.context.fillStyle = '#cccccc';
-
-    // 2
-    // let snakeHeadBdm = game.add.graphics();
-    // snakeHeadBdm.beginFill(0xDD9BB33); // (color, alpha) alpha is optional
-    // snakeHeadBdm.drawRect(300, 300, 16, 16); // (x, y, width, height)
-    
-    // 3
-    // snakeHeadBdm = game.add.bitmapData(gameWidth,gameHeight);            
-    // snakeHeadBdm.context.fillStyle = '#cccccc';
-
-    // 4 
-    // create a new bitmap data object
-    var snakeHeadBdm = game.add.bitmapData(1,1);
-
-    // draw to the canvas context like normal
-    snakeHeadBdm.ctx.beginPath();
-    snakeHeadBdm.ctx.rect(0,0,1,1);
-    snakeHeadBdm.ctx.fillStyle = '#cccccc';
-    snakeHeadBdm.ctx.fill();
-
-    // use the bitmap data as the texture for the sprite
- 
-    
-    snakeHead = game.add.sprite(400, 300, snakeHeadBdm);
-    snakeHead.anchor.setTo(0.5, 0.5);
-
-    // we are makingj bitmap data out of ...
-    bmd = game.add.bitmapData(gameWidth, gameHeight);
-
-    // it's filled with gray
-    bmd.context.fillStyle = '#cccccc';
-
-    // now it's a sprite
-    bg = game.add.sprite(0, 0, bmd);
-
-    // we're giving the game gravity
-    game.physics.arcade.gravity.y = 100;
-
-    // another sprite joines
-    hook = game.add.sprite(32, 450, 'arrow');
-    
-    // position the anchor
-    hook.anchor.set(0.5);
-
-
-    game.physics.enable(hook, Phaser.Physics.ARCADE);
-    
-
-    hook.body.collideWorldBounds = true;
-    hook.body.bounce.set(0.8);
-
-
-
-
-
-
-    (function () { // gamepad indicator
-        indicator = game.add.sprite(10,10, 'controller-indicator');
-        indicator.scale.x = indicator.scale.y = 2;
-        indicator.animations.frame = 1;
-        game.input.gamepad.start();
-         // To listen to buttons from a specific pad listen directly on that pad game.input.gamepad.padX, where X = pad 1-4
-        pad1 = game.input.gamepad.pad1;
-        // Stretch to fill
-        // game.input.onDown.add(dump, this);
-        // game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL; // Maintain aspect ratio
-        // game.input.onDown.add(gofull, this);
-    }());   
-    
-
-    (function () { // yellow square and line
-
-        let spriteX = 50;
-        let spriteY = 200;
-
-        // Grapple1 ------------------------------------------------------------------------------------------------------------------------------------------------------
-        grappleBMD = game.add.bitmapData(0,0);                
-        grappleBMD.ctx.beginPath();        
-        grappleBMD.ctx.lineWidth = "1";        
-        grappleBMD.ctx.strokeStyle = 'silver';        
-        grappleBMD.ctx.setLineDash([1]);        
-        grappleBMD.ctx.moveTo(spriteX, spriteY);        
-        grappleBMD.ctx.lineTo(400 , 400);        
-        grappleBMD.ctx.stroke();
-        grappleBMD.ctx.closePath();
-        grapple1 = game.add.sprite(0, 0, grappleBMD);
-
-        let square = game.add.graphics();
-        square.beginFill(0xDD9BB33); // (color, alpha) alpha is optional
-        square.drawRect(spriteX, spriteY, 16, 16); // (x, y, width, height)
-
-        sprites = game.add.group();
-        sprites.add(grapple1);
-        sprites.add(square);
-        sprite = sprites;
-    }());
-
-
-    
-    (function () { // trailing line
+        // =========================================================================================================
         
-        game.input.onDown.add(launch, this); // ????
+        var hookBmd = game.add.bitmapData(4,4);
+        hookBmd.ctx.beginPath();
+        hookBmd.ctx.rect(0,0,4,4);
+        hookBmd.ctx.fillStyle = '#ff0000';
+        hookBmd.ctx.fill();
+
+        hookSprite = game.add.sprite(0, 0, hookBmd);
+        hookSprite.anchor.setTo(0, 0);
+        game.physics.enable(hookSprite, Phaser.Physics.ARCADE);
+        hookSprite.body.collideWorldBounds = true;
+        hookSprite.body.checkCollision.up = false;
+        hookSprite.body.checkCollision.down = false;
+        hookSprite.body.immovable = true;
+        hookSprite.body.gravity.y = 600;
+
+        var chainLinkBmd = game.add.bitmapData(4,4);
+        chainLinkBmd.ctx.beginPath();
+        chainLinkBmd.ctx.rect(0,0,4,4);
+        chainLinkBmd.ctx.fillStyle = '#cccccc';
+        chainLinkBmd.ctx.fill();
+
+        var squareBmd = game.add.bitmapData(16,16);
+        squareBmd.ctx.beginPath();
+        squareBmd.ctx.rect(0,0,16,16);
+        squareBmd.ctx.fillStyle = '#DD9B33';
+        squareBmd.ctx.fill();
+
+        square = game.add.sprite(0, 0, squareBmd);
+        square.anchor.setTo(0, 0);
+        game.physics.enable(square, Phaser.Physics.ARCADE);
+        square.body.collideWorldBounds = true;
+        square.body.checkCollision.up = false;
+        square.body.checkCollision.down = false;
+        square.body.immovable = true;
+        // square.body.gravity.y = 600;
+
+
+
+        heroGroup = game.add.group();
+        heroGroup.add(hookSprite);
+        heroGroup.add(square);
+        heroGroup.x = 400;
+        heroGroup.y = 300;
         
-        function launch() {
-            if (game.input.x < hook.x) {
-                hook.body.velocity.setTo(-200, -200);
-            } else {
-                hook.body.velocity.setTo(200, -200);
+
+        //  Init chainLink array
+        for (var i = 1; i <= numChainLinks; i++) {
+            chainLink[i] = game.add.sprite(4, 4, chainLinkBmd);
+            chainLink[i].anchor.setTo(0, 0);
+            chainLink[i].alpha = 0;
+            
+            game.physics.enable(chainLink[i], Phaser.Physics.ARCADE);
+            chainLink[i].body.gravity.y = 600;
+            heroGroup.add(chainLink[i]);
+        }
+        
+        //  Init chainPath array
+        for (var i = 0; i <= numChainLinks; i++) {
+            chainPath[i] = new Phaser.Point(0, 0);
+        }
+    
+
+        (function () { // gamepad indicator
+            indicator = game.add.sprite(10,10, 'controller-indicator');
+            indicator.scale.x = indicator.scale.y = 2;
+            indicator.animations.frame = 1;
+        }());       
+    },
+    update: function() {
+
+        var launchGrappleButton = pad1.getButton(Phaser.Gamepad.XBOX360_RIGHT_BUMPER);
+        launchGrappleButton.onDown.add(launchGrapple, this);
+        launchGrappleButton.onUp.add(makeGrappleDisapear, this);
+
+        function launchGrapple() { // Will only be called once per key press. // Will be passed the full Key object. See Phaser.Key for properties.}
+            grappleLaunch = true;
+        }
+
+
+        if (grappleLaunch == true  && hookSprite.x <= numChainLinks * 5) {
+            launch();
+        } else {
+            makeGrappleDisapear();
+        }
+        
+
+        function makeGrappleDisapear() {
+            hookSprite.body.velocity.setTo(0, 0);
+            hookSprite.x = 12;
+            hookSprite.y = 0;
+            grappleLaunch = false;
+
+            for (var i = 1; i <= numChainLinks; i++) {
+                chainLink[i].x = hookSprite.x;
+                chainLink[i].y = hookSprite.y;
+                chainLink[i].alpha = 0;
+            }
+            for (var i = 0; i <= numChainLinks; i++) {
+                chainPath[i].x = hookSprite.x;
+                chainPath[i].y = hookSprite.y;
             }
         }
-    }());
+
+        function launch() { // launch 
+            hookSprite.body.velocity.setTo(300, -300);
+
+            // insert the new location at the start of the array, 
+            // and knock the last position off the end
+            var part = chainPath.pop();
+            part.setTo(hookSprite.x, hookSprite.y);
+            chainPath.unshift(part);
+
+            for (var i = 1; i <= numChainLinks; i++){
+                chainLink[i].x = (chainPath[i]).x;
+                chainLink[i].y = (chainPath[i]).y;
+                chainLink[i].body.velocity.setTo(300, -300);
+                chainLink[i].alpha = 1;
+            }
+        } // end launch
+        
+
+        
 
 
-   
 
-
-
-game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
-cursors = game.input.keyboard.createCursorKeys();
-
-
-
-
-    //  Init snakeSection array
-    for (var i = 1; i <= numSnakeSections; i++)
-    {
-        snakeSection[i] = game.add.sprite(1, 1, snakeHeadBdm);
-        snakeSection[i].anchor.setTo(1, 1);
-    }
-    
-    //  Init snakePath array
-    for (var i = 0; i <= numSnakeSections; i++)
-    {
-        snakePath[i] = new Phaser.Point(1, 1);
-    }
-    
-
-
-    
-}
-function update() {
-
-    snakeHead.body.velocity.setTo(0, 0);
-    snakeHead.body.angularVelocity = 0;
-
-    hook.rotation = hook.body.angle;
-    bmd.context.fillRect(hook.x, hook.y, 1, 1);
-
-    // If dirty this BitmapData will be re-rendered.
-    bmd.dirty = true;
-    // snakeHeadBdm.dirty = true;
+        
 
 
 
 
-if (cursors.up.isDown) {
-        snakeHead.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(snakeHead.angle, 300));
-
-        // Everytime the snake head moves, insert the new location at the start of the array, 
-        // and knock the last position off the end
-
-        var part = snakePath.pop();
-
-        part.setTo(snakeHead.x, snakeHead.y);
-
-        snakePath.unshift(part);
-
-        for (var i = 1; i <= numSnakeSections - 1; i++)
+            // Pad "connected or not" indicator
+        if (game.input.gamepad.supported && game.input.gamepad.active && pad1.connected)
         {
-            snakeSection[i].x = (snakePath[i]).x;
-            snakeSection[i].y = (snakePath[i]).y;
+            indicator.animations.frame = 0;
         }
-}
-
-if (cursors.left.isDown)
-{
-    snakeHead.body.angularVelocity = -300;
-}
-else if (cursors.right.isDown)
-{
-    snakeHead.body.angularVelocity = 300;
-}
-
-
- (function () { // gamepad 
-        // Pad "connected or not" indicator
-    if (game.input.gamepad.supported && game.input.gamepad.active && pad1.connected)
-    {
-        indicator.animations.frame = 0;
-    }
-    else
-    {
-        indicator.animations.frame = 1;
-    }
-
-    // Controls
-    if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
-    {
-        sprite.x--;
-    }
-    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
-    {
-        sprite.x++;
-    }
-
-    if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1)
-    {
-        sprite.y--;
-    }
-    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1)
-    {
-        sprite.y++;
-    }
-
-    if (pad1.justPressed(Phaser.Gamepad.XBOX360_A))
-    {
-        sprite.angle += 5;
-    }
-
-    if (pad1.justReleased(Phaser.Gamepad.XBOX360_B))
-    {
-        sprite.scale.x += 0.01;
-        sprite.scale.y = sprite.scale.x;
-    }
-
-    if (pad1.connected)
-    {
-        var rightStickX = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
-        var rightStickY = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
-
-        if (rightStickX)
+        else
         {
-            sprite.x += rightStickX * 10;
+            indicator.animations.frame = 1;
         }
 
-        if (rightStickY)
+
+
+
+
+
+        // Controls
+        if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
         {
-            sprite.y += rightStickY * 10;
+            heroGroup.x--;
         }
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
+        {
+            heroGroup.x++;
+        }
+
+        if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1)
+        {
+            heroGroup.y--;
+        }
+        else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1)
+        {
+            heroGroup.y++;
+        }
+
+        if (pad1.justPressed(Phaser.Gamepad.XBOX360_A))
+        {
+            
+        }
+
+        if (pad1.justReleased(Phaser.Gamepad.XBOX360_B))
+        {
+        
+        }
+
+        if (pad1.connected)
+        {
+            var rightStickX = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+
+            if (rightStickX)
+            {
+                heroGroup.x += rightStickX * 10;
+            }
+
+            if (rightStickY)
+            {
+                heroGroup.y += rightStickY * 10;
+            }
+        }
+    },
+    render: function() {
+
+        // game.debug.text(pad1)
+        // game.debug.geom(line);
+        // game.debug.geom(line, '#ffd900');
+        // game.debug.lineInfo(line, 32, 32);
+
+    },
+    gofull: function() {
+
+        if (game.scale.isFullScreen)
+        {
+            game.scale.stopFullScreen();
+        }
+        else
+        {
+            game.scale.startFullScreen(false);
+        }
+
+    },
+    dump: function() {
+
+        console.log(pad1._axes[0]);
+        console.log(pad1._rawPad.axes[0]);
+
     }
-    }());   
-
-
-    
-
-
-
-
-
-
-
 }
-function render() {
 
-    // game.debug.text(pad1)
-    // game.debug.geom(line);
-    // game.debug.geom(line, '#ffd900');
-    // game.debug.lineInfo(line, 32, 32);
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'phaser-example', {}, transparent, antialias);
+game.state.add('main', mainState);  
+game.state.start('main');
 
-}
-function gofull() {
 
-    if (game.scale.isFullScreen)
-    {
-        game.scale.stopFullScreen();
-    }
-    else
-    {
-        game.scale.startFullScreen(false);
-    }
-
-}
-function dump() {
-
-    console.log(pad1._axes[0]);
-    console.log(pad1._rawPad.axes[0]);
-
-}
--- src/main.js
+++ src/main.js
@@ -1,129 +1,116 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
-
-
-
-
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
+    
 
 }
+
+var hookSprite;
+var heroSprite;
+var groundSprite;
+var cursors;
+var platforms;
+var jumpButton;
 
 function create() {
 
-    game.stage.backgroundColor = "#4488AA";
-    game.physics.startSystem(Phaser.Physics.P2JS);
-    
-            
-    game.physics.p2.gravity.y = 1200;
 
-    //  Length, xAnchor, yAnchor
-    createRope(16, 400, 0);
+	//	Enable p2 physics
+	game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 800;
 
 
+    //  Add 2 sprites which we'll join with a spring
+    var hookBmd = game.add.bitmapData(4,4);
+        hookBmd.ctx.beginPath();
+        hookBmd.ctx.rect(0,0,4,4);
+        hookBmd.ctx.fillStyle = '#ff0000';
+        hookBmd.ctx.fill();
+        hookSprite = game.add.sprite(400, 300, hookBmd);
+        game.physics.p2.enable(hookSprite);
+
+    var heroBmd = game.add.bitmapData(16,16);
+        heroBmd.ctx.beginPath();
+        heroBmd.ctx.rect(0,0,16,16);
+        heroBmd.ctx.fillStyle = '#DD9B33';
+        heroBmd.ctx.fill();
+        heroSprite = game.add.sprite(400, 300, heroBmd);
+        // needed for collision detection
+        game.physics.enable(heroSprite, Phaser.Physics.ARCADE);
+        heroSprite.body.bounce.y = 0.2;
+        heroSprite.body.collideWorldBounds = true;
+	    // heroSprite.body.bounce.setTo(1, 1);
+        heroSprite.body.collideWorldBounds = true;
+        // heroSprite.body.setSize(20, 32, 5, 16);
+        game.physics.p2.enable(heroSprite);
+
+    var groundBmd = game.add.bitmapData(16,16);
+        groundBmd.ctx.beginPath();
+        groundBmd.ctx.rect(0,0,16,16);
+        groundBmd.ctx.fillStyle = '#DD9B33';
+        groundBmd.ctx.fill();
+        groundSprite = game.add.sprite(300, 400, groundBmd);
+        
+        // needed for collision detection
+        game.physics.enable(groundSprite, Phaser.Physics.ARCADE);
+        groundSprite.body.collideWorldBounds = true;
+        groundSprite.body.checkCollision.up = true;
+        groundSprite.body.checkCollision.down = false;
+        groundSprite.body.immovable = true;
+
+
+    // P2 physics
+    var distance = 150;
+    var localAnchorA = [0,0];
+    var localAnchorB = [0,0];
+    var maxForce = 400000;
+    var constraint = game.physics.p2.createDistanceConstraint(heroSprite,hookSprite, distance);
+
+
+    // controls
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 
 
-
-            
-    
 
 }
 
-function createRope(length, xAnchor, yAnchor) {
-
-    var lastRect;
+function update() {
     
-    var maxForce = 20000;   //  The force that holds the rectangles together.
-    var newRect;
+    game.physics.arcade.collide(heroSprite, groundSprite);
 
-    var height = 20;        //  Height for the physics body - your image height is 8px
-    var width = 21;         //  This is the width for the physics body. If too small the rectangles will get scrambled together.
-    var newRectThing = -(height);
-    var lastRectThing = height;
-    var chainLinkBmpHeight = height * 2;
-    var chainLinkBmpWidth = height * 2;
-    
-    var chainLinkBmd = game.add.bitmapData(chainLinkBmpWidth,chainLinkBmpHeight);
-    chainLinkBmd.ctx.beginPath();
-    chainLinkBmd.ctx.rect(0,0,chainLinkBmpWidth,chainLinkBmpHeight);
-    chainLinkBmd.ctx.fillStyle = '#cccccc';
-    chainLinkBmd.ctx.fill();
+    // Archade clollision detection breaks this 
+	// heroSprite.body.setZeroVelocity();
+    heroSprite.body.velocity.x = 0;
 
-    var chainLinkBmd2 = game.add.bitmapData(chainLinkBmpWidth,chainLinkBmpHeight);
-    chainLinkBmd2.ctx.beginPath();
-    chainLinkBmd2.ctx.rect(0,0,chainLinkBmpWidth,chainLinkBmpHeight);
-    chainLinkBmd2.ctx.fillStyle = '#333333';
-    chainLinkBmd2.ctx.fill();
-
-    var chain = game.add.group();
-    chain.scale.setTo(.1,.1);
-    chain.x = xAnchor
-    chain.y = yAnchor
-
-
-           
-
-
-    for (var i = 0; i <= length; i++)
+    if (cursors.left.isDown)
     {
-        var x = xAnchor;                    //  All rects are on the same x position
-        // var y = yAnchor + (i * height);     //  Every new rect is positioned below the last
-        var y = yAnchor;
-
-
-        if (i % 2 === 0)
-        {
-            //  Add sprite (and switch frame every 2nd time)
-            newRect = game.add.sprite(x, y, chainLinkBmd);
-            chain.add(newRect);
-         
-        }   
-        else
-        {
-                  
-            newRect = game.add.sprite(x, y, chainLinkBmd2);
-            chain.add(newRect);
-            lastRect.bringToTop();
-            chain.add(lastRect);
-        }
-
-        //  Enable physicsbody
-        game.physics.p2.enable(newRect, false);
-
-        //  Set custom rectangle
-        newRect.body.setRectangle(width, height);
-
-        if (i === 0)
-        {
-            newRect.body.static = true;
-        }
-        else
-        {  
-            //  Anchor the first one created
-            newRect.body.velocity.x = 400;      //  Give it a push :) just for fun
-            // newRect.body.velocity.y = -300;
-            // newRect.body.velocity.setTo(300, -300);
-            newRect.body.mass = (length) / i;     //  Reduce mass for evey rope element
-            console.log(newRect.body.mass)
-        }
-
-        //  After the first rectangle is created we can add the constraint
-        if (lastRect)
-        {
-            game.physics.p2.createRevoluteConstraint(newRect, [0, newRectThing], lastRect, [0, lastRectThing], maxForce
-            );
-        }
-
-        lastRect = newRect;
-
+    	// heroSprite.body.moveLeft(400);
+        heroSprite.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown)
+    {
+    	// heroSprite.body.moveRight(400);
+        heroSprite.body.velocity.x = 150;
     }
 
-          
-            
-            // game.physics.p2.createRevoluteConstraint(newRect, lastRect, {
-            //     worldPivot: [-1.5, 0]
-            // });
+    if (cursors.up.isDown)
+    {
+        // heroSprite.body.moveUp(400);
+    }
+    else if (cursors.down.isDown)
+    {
+        // heroSprite.body.moveDown(400);
+    }
 
+    if (jumpButton.isDown && heroSprite.body.onFloor())
+    {
+        heroSprite.body.velocity.y = -350;
+        // jumpTimer = game.time.now + 750;
+    }
 }
+
