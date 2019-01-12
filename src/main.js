8ad6eedeba283d3c46d18474105e023cdfbb4fd1
markDub77 <markwilliams1977@gmail.com>
2019-01-11 18:05:36 -0600

    Walking with hook

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { create: create, update: update });
 
-function preload() {
 
-    // game.load.image('stars', 'assets/misc/starfield.jpg');
-    // game.load.spritesheet('ship', 'assets/sprites/humstar.png', 32, 32);
-    // game.load.image('panda', 'assets/sprites/spinObj_01.png');
-    // game.load.image('sweet', 'assets/sprites/spinObj_06.png');
 
-}
-
-var ship;
-var panda;
+var heroSprite;
+var hookSprite;
 var starfield;
 var cursors;
 
@@ -24,58 +17,19 @@ function create() {
 
     game.physics.p2.restitution = 0.8;
 
-    //  Create our collision groups. One for the player, one for the pandas
-    var playerCollisionGroup = game.physics.p2.createCollisionGroup();
-    var pandaCollisionGroup = game.physics.p2.createCollisionGroup();
+    //  Create our collision groups. One for the heroSprite, one for the hookSprites
+    var heroSpriteCollisionGroup = game.physics.p2.createCollisionGroup();
+    var hookSpriteCollisionGroup = game.physics.p2.createCollisionGroup();
 
     //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
     //  (which we do) - what this does is adjust the bounds to use its own collision group.
     game.physics.p2.updateBoundsCollisionGroup();
 
-    // starfield = game.add.tileSprite(0, 0, 800, 600, 'stars');
-    // starfield.fixedToCamera = true;
-
-
-    
-    
-
-
-    for (var i = 0; i < 160; i++)
-    {
 
         
-var hookBmd = game.add.bitmapData(4,4);
-        hookBmd.ctx.beginPath();
-        hookBmd.ctx.rect(0,0,4,4);
-        hookBmd.ctx.fillStyle = '#cccccc';
-        hookBmd.ctx.fill();
-    panda = game.add.sprite(game.world.randomX, game.world.randomY, hookBmd);
-
-    // var pandas = game.add.group();
-    // pandas.enableBody = true;
-    panda.physicsBodyType = Phaser.Physics.P2JS;
-    game.physics.p2.enable(panda, false);
-    panda.body.setRectangle(4, 4);
     
-    panda.body.collides([pandaCollisionGroup, playerCollisionGroup]);
-    panda.body.velocity.x = 400;
-    panda.body.velocity.y = 400;
-    panda.body.setCollisionGroup(pandaCollisionGroup);
     
 
-    //     var panda = pandas.create(game.world.randomX, game.world.randomY, 'hookBmd2');
-    //     panda.body.setRectangle(40, 40);
-
-    //     //  Tell the panda to use the pandaCollisionGroup 
-    //     panda.body.setCollisionGroup(pandaCollisionGroup);
-
-    //     //  Pandas will collide against themselves and the player
-    //     //  If you don't set this they'll not collide with anything.
-    //     //  The first parameter is either an array or a single collision group.
-    //     panda.body.collides(pandaCollisionGroup);
-    //     panda.body.velocity.x = 500;
-    //     panda.body.velocity.y = 500;
-    }
 
 
 
@@ -86,81 +40,108 @@ var hookBmd = game.add.bitmapData(4,4);
 
 
 
-    //  Create our ship sprite
+    //  Create our heroSprite sprite
     var heroBmd = game.add.bitmapData(16,16);
         heroBmd.ctx.beginPath();
         heroBmd.ctx.rect(0,0,16,16);
         heroBmd.ctx.fillStyle = '#DD9B33';
         heroBmd.ctx.fill();
-    ship = game.add.sprite(200, 200, heroBmd);
-    // ship = game.add.sprite(200, 200, 'ship');
-    // ship.scale.set(2);
-    ship.smoothed = false;
-    ship.animations.add('fly', [0,1,2,3,4,5], 10, true);
-    ship.play('fly');
+    heroSprite = game.add.sprite(200, 200, heroBmd);
+        
+        game.physics.p2.enable(heroSprite, false);
+        heroSprite.body.setCircle(28);
+        heroSprite.body.fixedRotation = true;
 
-    game.physics.p2.enable(ship, false);
-    ship.body.setCircle(28);
-    ship.body.fixedRotation = true;
+    //  Set the heroSprites collision group
+    heroSprite.body.setCollisionGroup(heroSpriteCollisionGroup);
 
-    //  Set the ships collision group
-    ship.body.setCollisionGroup(playerCollisionGroup);
+    //  The heroSprite will collide with the hookSprites, and when it strikes one the hithookSprite callback will fire, causing it to alpha out a bit
+    //  When hookSprites collide with each other, nothing happens to them.
+    heroSprite.body.collides(hookSpriteCollisionGroup);
 
-    //  The ship will collide with the pandas, and when it strikes one the hitPanda callback will fire, causing it to alpha out a bit
-    //  When pandas collide with each other, nothing happens to them.
-    ship.body.collides(pandaCollisionGroup);
 
-    game.camera.follow(ship);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var hookBmd = game.add.bitmapData(4,4);
+        hookBmd.ctx.beginPath();
+        hookBmd.ctx.rect(0,0,4,4);
+        hookBmd.ctx.fillStyle = '#cccccc';
+        hookBmd.ctx.fill();
+    hookSprite = game.add.sprite(heroSprite.body.x, heroSprite.body.y, hookBmd);
+    // hookSprite.physicsBodyType = Phaser.Physics.P2JS;
+    game.physics.p2.enable(hookSprite, false);
+    hookSprite.body.setRectangle(4, 4);
+    
+    
+    // hookSprite.body.velocity.x = 400;
+    // hookSprite.body.velocity.y = 400;
+    hookSprite.body.setCollisionGroup(hookSpriteCollisionGroup);
+    hookSprite.body.collides([hookSpriteCollisionGroup]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     cursors = game.input.keyboard.createCursorKeys();
 
 }
 
-function hitPanda(body1, body2) {
-
-    //  body1 is the space ship (as it's the body that owns the callback)
-    //  body2 is the body it impacted with, in this case our panda
-    //  As body2 is a Phaser.Physics.P2.Body object, you access its own (the sprite) via the sprite property:
-    body2.sprite.alpha -= 0.1;
-
-}
 
 function update() {
 
-    ship.body.setZeroVelocity();
+
+
+    heroSprite.body.setZeroVelocity();
+    hookSprite.body.setZeroVelocity();
 
     if (cursors.left.isDown)
     {
-        ship.body.moveLeft(200);
+        heroSprite.body.moveLeft(200);
+        hookSprite.body.moveLeft(200);
     }
     else if (cursors.right.isDown)
     {
-        ship.body.moveRight(200);
+        heroSprite.body.moveRight(200);
+        hookSprite.body.moveRight(200);
     }
 
     if (cursors.up.isDown)
     {
-        ship.body.moveUp(200);
+        heroSprite.body.moveUp(200);
+        hookSprite.body.moveUp(200);
     }
     else if (cursors.down.isDown)
     {
-        ship.body.moveDown(200);
+        heroSprite.body.moveDown(200);
+        hookSprite.body.moveDown(200);
     }
-
-    if (!game.camera.atLimit.x)
-    {
-        // starfield.tilePosition.x += (ship.body.velocity.x * 16) * game.time.physicsElapsed;
-    }
-
-    if (!game.camera.atLimit.y)
-    {
-        // starfield.tilePosition.y += (ship.body.velocity.y * 16) * game.time.physicsElapsed;
-    }
-
 }
 
-function render() {
-
-    game.debug.text('Collide with the Pandas!', 32, 32);
-
-}
